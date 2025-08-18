b961153f543990ac3e916fe7c417b710d05fc7f0
Elwaen26 <72935068+Elwaen26@users.noreply.github.com>
2025-08-18 13:04:08 +0200

    Initial commit

---
+++ PersistentEmpiresLib/PersistentEmpiresLib/PersistentEmpiresMission/MissionBehaviors/AdminClientBehavior.cs
@@ -0,0 +1,109 @@
+﻿using PersistentEmpiresLib.Helpers;
+using PersistentEmpiresLib.NetworkMessages.Client;
+using PersistentEmpiresLib.NetworkMessages.Server;
+using PersistentEmpiresLib.SceneScripts;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+namespace PersistentEmpiresLib.PersistentEmpiresMission.MissionBehaviors
+{
+    public class AdminClientBehavior : MissionNetwork
+    {
+        public delegate void AdminPanelClick();
+        public event AdminPanelClick OnAdminPanelClick;
+        public static List<AdminTp> AdminTps = new List<AdminTp>();
+#if SERVER
+        public static bool CanUseSuicide = true;
+#endif
+
+        public void HandleAdminPanelClick()
+        {
+            if (this.OnAdminPanelClick != null)
+            {
+                this.OnAdminPanelClick();
+            }
+        }
+
+        public void HandleUnbanPlayerClick()
+        {
+            InformationManager.ShowTextInquiry(
+             new TextInquiryData(
+             GameTexts.FindText("AdminClientBehaviorInqCaption", null).ToString()
+             , GameTexts.FindText("AdminClientBehaviorInqText", null).ToString()
+             , true
+             , true
+             , GameTexts.FindText("PE_InquiryData_Select", null).ToString()
+             , GameTexts.FindText("PE_InquiryData_Cancel", null).ToString()
+             , OnIdWritten
+             , (Action)null
+             , false
+             , IsNameApplicable
+             , ""
+             , ""));
+        }
+        
+        private void OnIdWritten(string name)
+        {
+            var message = new RequestUnBan(GameNetwork.MyPeer.VirtualPlayer?.ToPlayerId(), name);
+            GameNetwork.BeginModuleEventAsClient();
+            GameNetwork.WriteMessage(message);
+            GameNetwork.EndModuleEventAsClient();
+        }
+
+        private Tuple<bool, string> IsNameApplicable(string inputText)
+        {
+            var reg = new Regex(@"^[0-9.]+$");
+            var result = reg.Match(inputText);
+
+            if (!result.Success)
+                return new Tuple<bool, string>(false, GameTexts.FindText("AdminClientBehaviorError1", null).ToString());
+
+            if (string.IsNullOrWhiteSpace(inputText))
+                return new Tuple<bool, string>(false, GameTexts.FindText("AdminClientBehaviorError2", null).ToString());
+
+            if (inputText.Length > 30)
+                return new Tuple<bool, string>(false, GameTexts.FindText("AdminClientBehaviorError3", null).ToString());
+
+            return new Tuple<bool, string>(true, inputText);
+        }
+
+        public override void OnBehaviorInitialize()
+        {
+            base.OnBehaviorInitialize();
+            this.AddRemoveMessageHandlers(GameNetwork.NetworkMessageHandlerRegisterer.RegisterMode.Add);
+#if SERVER
+            CanUseSuicide = ConfigManager.GetBoolConfig("CanUseSuicide", true);
+#endif
+        }
+        public override void OnRemoveBehavior()
+        {
+            AdminTps.Clear();
+            base.OnRemoveBehavior();
+            this.AddRemoveMessageHandlers(GameNetwork.NetworkMessageHandlerRegisterer.RegisterMode.Remove);
+        }
+
+        public void AddRemoveMessageHandlers(GameNetwork.NetworkMessageHandlerRegisterer.RegisterMode mode)
+        {
+            GameNetwork.NetworkMessageHandlerRegisterer networkMessageHandlerRegisterer = new GameNetwork.NetworkMessageHandlerRegisterer(mode);
+            if (GameNetwork.IsClient)
+            {
+                networkMessageHandlerRegisterer.Register<AuthorizeAsAdmin>(this.HandleAuthorizeAsAdminFromServer);
+            }
+        }
+
+        private void HandleAuthorizeAsAdminFromServer(AuthorizeAsAdmin message)
+        {
+            GameNetwork.MyPeer.GetComponent<PersistentEmpireRepresentative>().IsAdmin = true;
+        }
+
+
+        internal static void Register(AdminTp adminTp)
+        {
+            AdminTps.Add(adminTp);
+        }
+    }
+}
---
+++ PersistentEmpiresServer/PersistentEmpiresServer/ServerMissions/ChatCommandSystem.cs
@@ -0,0 +1,112 @@
+﻿using NetworkMessages.FromClient;
+using PersistentEmpiresHarmony.Patches;
+using PersistentEmpiresLib;
+using PersistentEmpiresLib.PersistentEmpiresMission.MissionBehaviors;
+using PersistentEmpiresServer.ChatCommands.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+namespace PersistentEmpiresServer.ServerMissions
+{
+    public class ChatCommandSystem : MissionNetwork
+    {
+        internal Dictionary<string, Command> commands;
+        public static ChatCommandSystem Instance;
+        internal bool DisableGlobalChat;
+        internal PatreonRegistryBehavior patreonRegistry;
+        internal Dictionary<NetworkCommunicator, bool> Muted;
+        public string CommandPrefix;
+        internal string DefaultMessageColor = "#FFFDFDFD";
+
+        public override void OnBehaviorInitialize()
+        {
+            base.OnBehaviorInitialize();
+            commands = new Dictionary<string, Command>();
+            Muted = new Dictionary<NetworkCommunicator, bool>();
+            Instance = this;
+            PatchGlobalChat.OnClientEventPlayerMessageAll += PatchGlobalChat_OnClientEventPlayerMessageAll;
+            LocalChatComponent localChat = base.Mission.GetMissionBehavior<LocalChatComponent>();
+            localChat.OnPrefixHandleLocalChatFromClient += this.OnPrefixHandleLocalChatFromClient;
+            patreonRegistry = base.Mission.GetMissionBehavior<PatreonRegistryBehavior>();
+            CommandPrefix = ConfigManager.GetStrConfig("MessagePrefix", "!");
+            DefaultMessageColor = ConfigManager.GetStrConfig("DefaultMessageColor", "#FFFDFDFD");
+
+            Initialize();
+        }
+
+        private bool OnPrefixHandleLocalChatFromClient(NetworkCommunicator Sender, string Message, bool shout)
+        {
+            PersistentEmpireRepresentative persistentEmpireRepresentative = Sender.GetComponent<PersistentEmpireRepresentative>();
+            if (Message.StartsWith(CommandPrefix))
+            {
+                string[] argsWithCommand = Message.Split(' ');
+                string command = argsWithCommand[0];
+                string[] args = argsWithCommand.Skip(1).ToArray();
+                this.Execute(Sender, command, args);
+                return false;
+            }
+            return true;
+        }
+
+        private bool PatchGlobalChat_OnClientEventPlayerMessageAll(NetworkCommunicator networkPeer, PlayerMessageAll message)
+        {
+            PersistentEmpireRepresentative persistentEmpireRepresentative = networkPeer.GetComponent<PersistentEmpireRepresentative>();
+            if (message.Message.StartsWith(CommandPrefix))
+            {
+                string[] argsWithCommand = message.Message.Split(' ');
+                string command = argsWithCommand[0];
+                string[] args = argsWithCommand.Skip(1).ToArray();
+                this.Execute(networkPeer, command, args);
+                return false;
+            }
+            else if (persistentEmpireRepresentative != null && persistentEmpireRepresentative.IsAdmin)
+            {
+                InformationComponent.Instance.BroadcastMessage("(Admin) " + networkPeer.GetComponent<MissionPeer>().DisplayedName + ": " + message.Message, Color.ConvertStringToColor("#FDD835FF").ToUnsignedInteger());
+                return false;
+            }
+            if (persistentEmpireRepresentative.IsAdmin || this.patreonRegistry.IsPlayerPatreon(networkPeer)) return true;
+            if (this.DisableGlobalChat) return false;
+            if (this.Muted.ContainsKey(networkPeer))
+            {
+                InformationComponent.Instance.SendMessage("You are muted.", Colors.Red.ToUnsignedInteger(), networkPeer);
+                return false;
+            }
+            return true;
+        }
+
+        public bool Execute(NetworkCommunicator networkPeer, string command, string[] args)
+        {
+            Command executableCommand;
+            bool exists = commands.TryGetValue(command, out executableCommand);
+            if (!exists)
+            {
+                InformationComponent.Instance.SendMessage("This command is not exists", Colors.Red.ToUnsignedInteger(), networkPeer);
+                return false;
+            }
+            if (!executableCommand.CanUse(networkPeer))
+            {
+                InformationComponent.Instance.SendMessage("You are not authorized to run this command", Colors.Red.ToUnsignedInteger(), networkPeer);
+                return false;
+            }
+            return executableCommand.Execute(networkPeer, args);
+        }
+
+        private void Initialize()
+        {
+            this.commands = new Dictionary<string, Command>();
+            foreach (Type mytype in System.Reflection.Assembly.GetExecutingAssembly().GetTypes()
+                 .Where(mytype => mytype.GetInterfaces().Contains(typeof(Command))))
+            {
+                Command command = (Command)Activator.CreateInstance(mytype);
+                if (!commands.ContainsKey(command.Command()) && command.IsEnabled())
+                {
+                    Debug.Print("** Chat Command " + command.Command() + " have been initiated !", 0, Debug.DebugColor.Green);
+                    commands.Add(command.Command(), command);
+                }
+            }
+        }
+    }
+}
